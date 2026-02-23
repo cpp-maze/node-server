1a3d15ead7d686f8edce5c62334c1dc9125d4f41
cpp-maze <165675882+cpp-maze@users.noreply.github.com>
2026-02-23 07:40:01 +0000

    added four lambda method

--- services/hello_world_put/app.py
+++ services/hello_world_put/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Santhoshkumar hello world",
+            "message": "PUT: hello world",
             "version": 'v2'
             # "location": ip.text.replace("\n", "")
         }),
